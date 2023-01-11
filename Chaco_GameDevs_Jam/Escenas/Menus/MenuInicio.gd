b774543638e2ceb3de020c608a84246ea08cb346
soyMoka <d15.cejas.matias@gmail.com>
2023-01-11 16:57:36 -0300

    Menu pausa para integrar

--- Chaco_GameDevs_Jam/Escenas/Menus/MenuInicio.gd
+++ Chaco_GameDevs_Jam/Escenas/Menus/MenuInicio.gd
@@ -1,21 +1,15 @@
 extends Control
 
 export var escena_juego = 'res://Escenas/Niveles/Nivel.tscn'
+export var creditos = "res://Escenas/Menus/Creditos.tscn"
 
-onready var continuar_button = $MarginContainer/VBoxContainer2/VBoxContainer/Continuar
-var continuar = ''
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	if continuar == '':
-		continuar_button.disabled = true
-	else: 
-		continuar_button.disabled = false
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -24,13 +18,14 @@ func _ready():
 
 func _on_Jugar_pressed():
 # warning-ignore:return_value_discarded
-	get_tree().change_scene(escena_juego) # Replace with function body.
+	get_tree().change_scene(escena_juego) 
 
 
-func _on_Continuar_pressed():
-# warning-ignore:return_value_discarded
-	get_tree().change_scene(continuar)
-
+func _on_Creditos_pressed():
+	get_tree().change_scene(creditos)
+	
 
 func _on_Salir_pressed():
 	get_tree().quit()
+
+
---
+++ Chaco_GameDevs_Jam/Escenas/Menus/MenuPausa.gd
@@ -0,0 +1,56 @@
+extends Control
+
+
+onready var mago1 = preload("res://Escenas/Player/magoLvl1.png") 
+onready var mago2 = preload("res://Escenas/Player/magoLvl2.png")
+onready var mago3 = preload("res://Escenas/Player/magoLvl3.png")
+onready var mago4 = preload("res://Escenas/Player/magoLvl4.png")
+onready var mago5 = preload("res://Escenas/Player/magoLvl5.png")
+onready var textura_mago = $Paper/HBoxContainer/TextureRect
+
+onready var stats_poder = $Paper/HBoxContainer/Stats/Poder
+onready var stats_ratio = $Paper/HBoxContainer/Stats/Ratio
+onready var stats_vida = $Paper/HBoxContainer/Stats/Vida
+onready var stats_velocidad = $Paper/HBoxContainer/Stats/Velocidad
+
+# esto es para cargar las estadisticas
+#onready var player_poder = $Paper/HBoxContainer/Stats/Poder
+#onready var player_ratio = $Paper/HBoxContainer/Stats/Ratio
+#onready var player_vida = $Paper/HBoxContainer/Stats/Vida
+#onready var plsyer_velocidad = $Paper/HBoxContainer/Stats/Velocidad
+
+export var menu_princiipal = "res://Escenas/Menus/MenuInicio.tscn"
+
+
+
+
+func _ready(): 
+	actualizarStats()
+	actualizarImgNivelMago()
+	
+	
+	
+	
+	
+	
+func actualizarStats():
+	## Esto es para actualizar los stats al poner pausa
+	## Hay que ponerle las stats correctas
+	stats_poder.text = '1'
+	stats_ratio.text = '1'
+	stats_vida.text = '00'
+	stats_velocidad.text = '00'
+
+func actualizarImgNivelMago():
+	textura_mago.texture = mago3
+	
+	
+	
+
+func _on_MenuPrincipal_pressed():
+	get_tree().change_scene(menu_princiipal)
+
+
+func _on_Continuar_pressed():
+	if get_tree().paused:
+		get_tree().paused = false
