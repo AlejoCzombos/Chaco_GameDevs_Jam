3465cf36165807a4baee571b7556ea2a71ced47c
Alejo <alejoczombos@gmail.com>
2023-01-13 19:49:42 -0300

    Enemigo huyendo

--- Chaco_GameDevs_Jam/Escenas/Enemigos/EnemigoCactus.gd
+++ Chaco_GameDevs_Jam/Escenas/Enemigos/EnemigoCactus.gd
@@ -1,2 +1,34 @@
 class_name EnemigoCactus
 extends EnemigoBase
+
+enum ESTADO_IA{ATACANDO, HUYENDO}
+
+var estadoIAactual = ESTADO_IA.HUYENDO
+
+var huyendo = false
+
+func _process(delta) -> void:
+	if player_objetivo != null && !stuneado && puede_moverse:
+		movimiento = -global_position.direction_to(player_objetivo.global_position)
+	elif stuneado:
+		movimiento = lerp(movimiento, Vector2(0,0), 0.3)
+	if huyendo:
+		global_position += movimiento * (velocidad  * 5 ) * delta
+
+func controladorEstadoIA(nuevoEstado: int) -> void:
+	match nuevoEstado:
+		ESTADO_IA.ATACANDO:
+			huyendo = false
+			puede_moverse = false
+		ESTADO_IA.HUYENDO:
+			puede_moverse = true
+			huyendo = true
+		_:
+			printerr("Error estados")
+	estadoIAactual = nuevoEstado
+
+func _on_AreaPersecusion_body_entered(body):
+	controladorEstadoIA(ESTADO_IA.HUYENDO)
+
+func _on_AreaSalirPersecusion_body_exited(body):
+	controladorEstadoIA(ESTADO_IA.ATACANDO)
