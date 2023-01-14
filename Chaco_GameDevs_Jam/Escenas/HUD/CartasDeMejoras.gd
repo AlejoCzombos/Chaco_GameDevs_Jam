d7f03e6999ed1ef0be315bd5f20256b5e71a5392
soyMoka <d15.cejas.matias@gmail.com>
2023-01-14 16:11:35 -0300

    Creación cartas de mejora (solo layaut)

---
+++ Chaco_GameDevs_Jam/Escenas/HUD/CartasDeMejoras.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+const mejoraImg:Array = [
+	"res://Escenas/Menus/Assets_Stats/fireRatio_icon.png", 		# Ratio de fuego
+	"res://Escenas/Menus/Assets_Stats/poder_icon.png",			# Daño
+	"res://Escenas/Menus/Assets_Stats/hp_icon.png",				# Vida
+	"res://Escenas/Menus/Assets_Stats/velocidad_icon.png"		# Velocidad
+]
+ 
+const mejoraDescripcion:Array=[
+	'Ratio mejorado', 		# Ratio de fuego
+	'Daño mejorado',				# Daño
+	'Vida mejorada',				# Vida
+	'Velocidad mejorada' ]			# Velocidad
+
+const cartas = {
+	0:[mejoraImg[0], mejoraDescripcion[0]],	# Ratio de fuego
+	1:[mejoraImg[1], mejoraDescripcion[1]],	# Daño
+	2:[mejoraImg[2], mejoraDescripcion[2]],	# Vida
+	3:[mejoraImg[3], mejoraDescripcion[3]],	# Velocidad
+}
+
