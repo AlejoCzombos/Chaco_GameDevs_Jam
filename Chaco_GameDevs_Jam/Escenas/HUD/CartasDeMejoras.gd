14c6e692725d2a7aba6525baa443553ea8a5494e
soyMoka <d15.cejas.matias@gmail.com>
2023-01-14 23:08:57 -0300

    agregados metodos de cartas de mejora

--- Chaco_GameDevs_Jam/Escenas/HUD/CartasDeMejoras.gd
+++ Chaco_GameDevs_Jam/Escenas/HUD/CartasDeMejoras.gd
@@ -1,23 +1,61 @@
 extends CanvasLayer
 
+onready var cardImg1 = $HBoxContainer/NinePatchRect/VBoxContainer/MarcoImg/ImgCartaMejora
+onready var cardImg2 = $HBoxContainer/NinePatchRect2/VBoxContainer/MarcoImg/ImgCartaMejora
+onready var cardDesc1 = $HBoxContainer/NinePatchRect/VBoxContainer/Label
+onready var cardDesc2 = $HBoxContainer/NinePatchRect2/VBoxContainer/Label
+
 
 const mejoraImg:Array = [
-	"res://Escenas/Menus/Assets_Stats/fireRatio_icon.png", 		# Ratio de fuego
-	"res://Escenas/Menus/Assets_Stats/poder_icon.png",			# Daño
-	"res://Escenas/Menus/Assets_Stats/hp_icon.png",				# Vida
-	"res://Escenas/Menus/Assets_Stats/velocidad_icon.png"		# Velocidad
+	preload("res://Escenas/Menus/Assets_Stats/fireRatio_icon.png"), 		# Ratio de fuego
+	preload("res://Escenas/Menus/Assets_Stats/poder_icon.png"),			# Daño
+	preload("res://Escenas/Menus/Assets_Stats/hp_icon.png"),				# Vida
+	preload("res://Escenas/Menus/Assets_Stats/velocidad_icon.png")		# Velocidad
 ]
  
 const mejoraDescripcion:Array=[
-	'Ratio mejorado', 		# Ratio de fuego
-	'Daño mejorado',				# Daño
-	'Vida mejorada',				# Vida
-	'Velocidad mejorada' ]			# Velocidad
+	'Ratio\nmejorado', 		# Ratio de fuego
+	'Daño\nmejorado',				# Daño
+	'Vida\nmejorada',				# Vida
+	'Velocidad\nmejorada' ]			# Velocidad
 
-const cartas = {
-	0:[mejoraImg[0], mejoraDescripcion[0]],	# Ratio de fuego
-	1:[mejoraImg[1], mejoraDescripcion[1]],	# Daño
-	2:[mejoraImg[2], mejoraDescripcion[2]],	# Vida
-	3:[mejoraImg[3], mejoraDescripcion[3]],	# Velocidad
-}
+const cartas = [
+	[mejoraImg[0], mejoraDescripcion[0]],	# Ratio de fuego
+	[mejoraImg[1], mejoraDescripcion[1]],	# Daño
+	[mejoraImg[2], mejoraDescripcion[2]],	# Vida
+	[mejoraImg[3], mejoraDescripcion[3]],	# Velocidad	
+]
 
+
+########### Prueba #############
+func _ready():
+	set_cards(0,1)
+	# 0 = Tario de fuego
+	# 1 = Daño
+	# 2 = Vida
+	# 3 = Velocidad
+##  (usar este metodo para setear las cartas)
+################################3
+
+func set_cards(tipoCarta1, tipoCarta2):
+	# 0 = Tario de fuego
+	# 1 = Daño
+	# 2 = Vida
+	# 3 = Velocidad
+	
+	var imagen 
+	var descripcion 
+	
+	imagen = cartas[tipoCarta1][0]
+	print(imagen)
+	descripcion = cartas[tipoCarta1][1]
+	print(descripcion)
+	cardImg1.set_texture(imagen)
+	cardDesc1.set_text(descripcion)
+	
+	imagen = cartas[tipoCarta2][0]
+	print(imagen)
+	descripcion = cartas[tipoCarta2][1]
+	print(descripcion)
+	cardImg2.set_texture(imagen)
+	cardDesc2.set_text(descripcion)
